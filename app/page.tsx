10f8174ac40e1e77814cbc4bf1d9b9cdf3588ca8
Marczelloo <moskwamarcel@gmail.com>
2025-11-19 20:49:38 +0100

    fix: update layout for Projects section to improve responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-16 px-4 sm:px-6 md:px-10 lg:px-24 max-w-7xl mx-auto">
+    <div className="flex flex-col gap-16 px-4 sm:px-6 md:px-10 lg:px-24 max-w-[1500px] mx-auto">
+      {" "}
       <Hero />
       <AboutMe />
       <Projects />
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,7 +8,8 @@ export default function Projects() {
         <h2 className="text-3xl md:text-4xl font-semibold text-(--accent-text)">My Projects</h2>
         <span className="hidden md:block h-px flex-1 bg-linear-to-r from-(--accent-text)/70 to-transparent" />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 mt-6 items-stretch">
+      <div className="grid grid-cols-1  sm:grid-cols-2  xl:grid-cols-3  2xl:grid-cols-4  gap-6 md:gap-8 mt-6 items-stretch">
+        {" "}
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
